14afb8a08d3bfd2ce040a749cb20e8de77d23948
Nicole Hartman <nicole22@stanford.edu>
2023-08-21 08:33:48 -0700

    start looking at the increasing T exps

--- code/runExperiments.py
+++ code/runExperiments.py
@@ -22,7 +22,7 @@ def writeSlurmFile(pythonCmd, job_name, useGPU=True):
         "ntasks": 1,
         "cpus-per-task": 1,
         "mem-per-cpu": "4g",
-        "time": "12:00:00",  # hour: minute: second
+        "time": "24:00:00",  # hour: minute: second
         # Specify the job name, and input and output logfiles
         "job-name": job_name,
         "output": f"log_files/{job_name}.log",
@@ -66,9 +66,36 @@ os.system(f"sbatch {SLURM_DIR}/{job}.sh")
 
 # Test the temperature
 #for ctag in ['muP-D','LHT','sqrtD']:
-for ctag in ['muP-D','sqrtD']:
-    cID = f'2rings-{ctag}'
+#for ctag in ['sqrtD']: # muP-D
+#    cID = f'2rings-{ctag}'
+#    cmd = f"python train.py --config configs/{cID}.yaml --device cuda:0"
+#    cmd += " --warm_start "
+#    writeSlurmFile(cmd, cID, useGPU=True)
+#    os.system(f"sbatch {SLURM_DIR}/{cID}.sh")
+#
+## Suggestion from Steffan: Decrease the query dim of SA
+#for Q in [4,8]:
+#    cID = f'2rings-sqrtD-Q{Q}'
+#    cmd = f"python train.py --config configs/{cID}.yaml --device cuda:0"
+#    cmd += " --warm_start "
+#    writeSlurmFile(cmd, cID, useGPU=True)
+#    os.system(f"sbatch {SLURM_DIR}/{cID}.sh")
+
+# 21.08.23 smaller Q dim didn't work as well, so let's go the other direction!
+#for Q in [16,32,64,128,256]:
+#for Q in [16,32,64]:
+#    cID = f'2rings-sqrtD-Q{Q}'
+#    cmd = f"python train.py --config configs/{cID}-10k-decay.yaml --device cuda:0"
+#    cmd += f" --warm_start --warm_start_config configs/{cID}.yaml"
+#    writeSlurmFile(cmd, f"{cID}-10k-decay", useGPU=True)
+#    os.system(f"sbatch {SLURM_DIR}/{cID}-10k-decay.sh")
+
+# Try increasing the # iters you're solving the problem in
+for T in [2,3,4,5]:
+    cID = f'2rings-T{T}'
     cmd = f"python train.py --config configs/{cID}.yaml --device cuda:0"
-    cmd += " --warm_start "
+    cmd += f" --warm_start --warm_start_config configs/2rings-sqrtD.yaml"
     writeSlurmFile(cmd, cID, useGPU=True)
     os.system(f"sbatch {SLURM_DIR}/{cID}.sh")
+
+
