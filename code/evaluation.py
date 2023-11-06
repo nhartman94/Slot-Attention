b7f033fc8f149172b4c6bd03e2444e7b20602516
Sara Aumiller <sara00au@gmail.com>
2023-11-06 16:15:46 +0100

    different alpha studies

--- code/evaluation.py
+++ code/evaluation.py
@@ -10,9 +10,13 @@ def student_t(Y_pred_sorted, Y_true_sorted, n_rings=2, bs=1000):
     div =  (Y_pred_sorted-Y_true_sorted).reshape(bs*n_rings, 3).T
     labels = [r"$x$ resolution", r"$y$ resolution", r"$R$ resolution"]
 
+    fit_params = np.zeros((3,3))
     for i, (ax, label) in enumerate(zip(axs[0:], labels)):
         # fit with student t
         df, loc, scale = t.fit(div[i])
+        fit_params[i, 0] = df
+        fit_params[i, 1] = loc
+        fit_params[i, 2] = scale
         label_fit= f" df={df:.3},\n mean={loc:.3},\n scale={scale:.3}"
         # plot
         n_counts, containers, patches = ax.hist(div[i], bins=100, density=True) #desity=True to return a probability density!
@@ -25,6 +29,7 @@ def student_t(Y_pred_sorted, Y_true_sorted, n_rings=2, bs=1000):
         ax.legend()
     plt.tight_layout()
     plt.show()
+    return fit_params
     
 def KL_divergence(slots_sorted, rings_sorted):
     l_kl = F.kl_div(torch.log(slots_sorted),rings_sorted,reduction='none').sum(axis=1).mean(axis=-1)
